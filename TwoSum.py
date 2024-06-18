18c00b029a64fd62d0d74e547d41a5a938017f2f
Justin Siek <justinsiek>
2024-06-18 10:37:50 -0600

    test

--- TwoSum.py
+++ TwoSum.py
@@ -1,6 +1,6 @@
 def twoSum(nums, target):
     hm = dict()
-        for i in range(len(nums)):
-            if nums[i] in hm:
-                return [hm[nums[i]], i]
-            hm[target - nums[i]] = i
+    for i in range(len(nums)):
+        if nums[i] in hm:
+            return [hm[nums[i]], i]
+        hm[target - nums[i]] = i
